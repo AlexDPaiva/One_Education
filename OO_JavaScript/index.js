0e0e3b145e48eed26ec5d2ebbec1544c996728b7
Alex Paiva <49997083+AlexDPaiva@users.noreply.github.com>
2022-07-31 19:07:08 -0300

    Add files via upload

--- OO_JavaScript/index.js
+++ OO_JavaScript/index.js
@@ -1,23 +1,41 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
 }
+
+class ContaCorrente{
+    agencia;
+    _saldo;
+
+    sacar(valor){
+      if(this._saldo >= valor){
+        this._saldo-= valor;
+        return  valor;
+      }
+    }
+    depositar(valor){
+      if (valor > 0){
+        this._saldo += valor;
+      }
+    }
+}
+
 
 const cliente01 = new Cliente();
 const cliente02 = new Cliente();
 
 cliente01.nome = "Alex";
 cliente01.cpf = 32322290807;
-cliente01.agencia = 1001;
-cliente01.saldo = 0.0;
 
 cliente02.nome = "Jennifer";
 cliente02.cpf = 09099933309;
-cliente02.agencia = 1001;
-cliente02.saldo = 0.0;
+
+let contaAlex = new ContaCorrente();
+contaAlex.saldo = 0;
+contaAlex.agencia = 1001;
+
+contaAlex.depositar(100.00)
+contaAlex.sacar(50.00)
 
 
-
-console.log(cliente01)
+console.log(`Oi ${cliente01} seu saldo é de ${contaAlex.saldo} reais.`);
